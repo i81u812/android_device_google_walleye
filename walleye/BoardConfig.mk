a8f77b2a84bafa449a31a0095325ff2c9665b745
Zips <hcolmenares@gmail.com>
2018-03-19 18:46:41 -0400

    walleye: Centralize all common product  to wahoo
    Change-Id: Id3d420cc80a873961ee56623abb208703621f19e

--- walleye/BoardConfig.mk
+++ walleye/BoardConfig.mk
@@ -20,7 +20,7 @@ DEFAULT_LOW_PERSISTENCE_MODE_BRIGHTNESS := 0x00000056
 include device/google/wahoo/BoardConfig.mk
 -include vendor/google/walleye/BoardConfigVendor.mk
 
-TARGET_KERNEL_CONFIG := lineageos_muskie_defconfig
+TARGET_KERNEL_CONFIG := flash-walleye_defconfig
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 
 #sepolicy common to muskie/walleye
